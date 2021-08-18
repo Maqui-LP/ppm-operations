566b8c06ec78d8080b0ad60f8c08bb0940073cff
Maqui-LP <maqui-lp@outlook.com>
2021-08-18 19:08:16 -0300

    Added tests

---
+++ bin/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change to repo's top level directory
+cd $(dirname "$0")/..
+
+gcc *.c -o main.exe
+chmod +x main.exe
+
+assert() {
+    echo "Running ./main.exe -i ./samples/model.ppm -o ./hidden/$2.ppm $3"
+    ./main.exe -i ./samples/model.ppm -o ./hidden/$2.ppm $3
+    diff ./samples/$1.ppm ./hidden/$2.ppm >/dev/null
+    if [[ $? -ne 0 ]]; then 
+        echo "Test failed for flag $3"
+        exit 1
+    fi
+    echo "Succeded!"
+    echo
+}
+
+assert "modelCopy" "modelCopy2"
+assert "modelNegative" "modelNegative2" "-n"
+assert "modelRotated" "modelRotated2" "-r"
+assert "modelHorizontalMirror" "modelHorizontalMirror2" "-h"
+assert "modelVerticalMirror" "modelVerticalMirror2" "-v"
+assert "modelBlur7" "modelBlur72" "-b 7"
