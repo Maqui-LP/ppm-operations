5a85b519fde98f9d4c7d4925308cc63070be95fb
Maqui-LP <maqui-lp@outlook.com>
2021-08-18 20:31:14 -0300

    fixed bug

--- bin/test.sh
+++ bin/test.sh
@@ -4,8 +4,8 @@
 cd $(dirname "$0")/..
 
 assert() {
-    echo "Running ./main.exe -i ./samples/model.ppm -o ./hidden/$2.ppm $3"
-    ./main.exe -i ./samples/model.ppm -o ./hidden/$2.ppm $3
+    echo "Running ./bin/ppm -i ./samples/model.ppm -o ./hidden/$2.ppm $3"
+    ./bin/ppm -i ./samples/model.ppm -o ./hidden/$2.ppm $3
     diff ./samples/$1.ppm ./hidden/$2.ppm >/dev/null
     if [[ $? -ne 0 ]]; then 
         echo "Test failed for flag $3"
